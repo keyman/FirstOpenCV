03bbc22d3e3b48dbbf431b73211ee0bdb36386ad
Keyman Kim <Keyman@Keymans-MacBook-Air.local>
2013-12-30 22:37:13 +0900

    OpenCV 프로그램 반영

--- FirstOpenCV/main.cpp
+++ FirstOpenCV/main.cpp
@@ -5,14 +5,33 @@
 //  Created by Keyman Kim on 12/23/13.
 //  Copyright (c) 2013 Keyman Kim. All rights reserved.
 //
+// Example showing how to read and write images
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+//#include <opencv/cvaux.hpp> //why compile error in this case?
+#include <opencv/cvaux.h>
 
-#include <iostream>
-
-int main(int argc, const char * argv[])
+int main(int argc, char** argv)
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    IplImage * pInpImg = 0;
+    
+    // Load an image from file - change this based on your image name
+    pInpImg = cvLoadImage("my_image.jpg", CV_LOAD_IMAGE_UNCHANGED);
+    if(!pInpImg)
+    {
+        fprintf(stderr, "failed to load input image\n");
+        return -1;
+    }
+    
+    // Write the image to a file with a different name,
+    // using a different image format -- .png instead of .jpg
+    if( !cvSaveImage("my_image_copy.png", pInpImg) )
+    {
+        fprintf(stderr, "failed to write image file\n");
+    }
+    
+    // Remember to free image memory after using it!
+    cvReleaseImage(&pInpImg);
+    
     return 0;
 }
-
